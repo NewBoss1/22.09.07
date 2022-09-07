b029c7f985b9aed93e6f7f3b4481e04fd59478c1
NewBoss1 <58598186+NewBoss1@users.noreply.github.com>
2022-09-07 22:47:16 +0900

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const age = parseInt(prompt("write your Number"));
 
 console.log(isNaN(age));
+//isNan은 값이 숫자인지 아닌지를 판별해준다. 숫자가 아니라면 true/숫자라면 false를 출력한다.
 
 if (isNaN(age)) {
   console.log("Please write your Number");
